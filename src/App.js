fbfe80324c60e5b2eacf91784a4094ed530b5eb4
Dimitris Stavroulakis <jimstav@gmail.com>
2024-10-31 19:52:07 +0200

    Adding Sass

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import "./categories.styles.scss";
+
 const App = () => {
   const categories = [
     {
@@ -25,7 +27,7 @@ const App = () => {
   return (
     <div className="categories-container">
       {categories.map(({ title, id }) => (
-        <div className="category-container" key={id}>
+        <div key={id} className="category-container">
           <div className="background-image" />
           <div className="category-body-container">
             <h2>{title}</h2>
