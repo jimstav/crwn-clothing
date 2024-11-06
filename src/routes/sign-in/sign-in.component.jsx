25d10115e237539a4e6825d3b406be4d088144de
Dimitris Stavroulakis <jimstav@gmail.com>
2024-11-06 14:55:50 +0200

    Generalizing Form Input Component

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
-import SignUpForm from "../../components/sign-up-form/sign-up-form.components";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
   const logGoogleUser = async () => {
