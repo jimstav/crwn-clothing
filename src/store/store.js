92875587a84e3ef22a255b07d7e43492af645a47
Dimitris Stavroulakis <jimstav@gmail.com>
2025-05-05 14:13:13 +0300

    React-Redux: Setting Up Our Store

--- src/store/store.js
+++ src/store/store.js
@@ -4,5 +4,10 @@ import {
   applyMiddleware,
 } from 'redux';
 import logger from 'redux-logger';
+import { rootReducer } from './root-reducer';
 
-// root-reducer
+const middleWares = [logger];
+
+const composedEnhancers = compose(applyMiddleware(...middleWares));
+
+export const store = createStore(rootReducer, undefined, composedEnhancers);
